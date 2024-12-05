0c0756310d7ad37fce67649bf4b8db4358f040db
sephturner <turnerj8@wit.edu>
2024-12-05 13:51:36 -0500

    cleaner

--- cypress/e2e/landing/01_load.cy.js
+++ cypress/e2e/landing/01_load.cy.js
@@ -1,7 +1,7 @@
-describe('homepage load', () => {
+describe('Landing Page Loads', () => {
 
   // First test case in all e2e tests: page must load for all test to move forward
-  it('homepage loads', () => {
+  it('Page Loads', () => {
     cy.visit('https://demoqa.com/');
   });
 
