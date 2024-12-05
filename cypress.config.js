b32365395e7fcfb7c747537ab523125e4c031094
sephturner <turnerj8@wit.edu>
2024-12-05 13:47:47 -0500

    npm cleaning

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,18 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+
+    // Reporter options
+    reporter: 'mochawesome',
+
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      reportFilename: 'report',
+      overwrite: true, // May change later
+      html: false,
+      json: true,
+    },
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
