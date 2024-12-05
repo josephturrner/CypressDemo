2d27cde8bcf720a5074575603cf0653c39b6701c
sephturner <turnerj8@wit.edu>
2024-12-05 13:56:58 -0500

    initial bookstore test

---
+++ cypress/e2e/books/01_load.cy.js
@@ -0,0 +1,8 @@
+describe('Book Store Loads', () => {
+
+    // First test case in all e2e tests: page must load for all test to move forward
+    it('Page Loads', () => {
+        cy.visit('https://demoqa.com/books');
+    });
+
+});
